986470faf5843ac55f9a95646bbdcd51d1a767e3
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-17 21:28:42 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (email) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+                },
+                message: `Please enter a valid email address`
+            },
+            thoughts: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Thought',
+                }
+            ],
+            friends: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User',
+                }
+            ]
+
+        },
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+)
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports: User;
